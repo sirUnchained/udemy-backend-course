6fbac456db527bb40c48a56a9ddce68bbbf3e516
sirunchained <bbehnam3831@gmail.com>
2025-11-28 22:28:32 +0330

    well i just installed godotenv and now i have access to .enc file?

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/sirUnchained/udemy-backend-course/internal/env"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln(err)
+		os.Exit(-1)
+	}
 	cfg := config{addr: env.GetString("ADDR", ":8000")}
 	app := &application{config: cfg}
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sirUnchained/udemy-backend-course
 
 go 1.25.4
 
-require github.com/go-chi/chi/v5 v5.2.3 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.3 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)

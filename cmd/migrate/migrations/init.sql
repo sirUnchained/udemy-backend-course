d1086e55fc13804430af2868ac91b2bbb7f54257
sirunchained <bbehnam3831@gmail.com>
2025-12-02 16:16:10 +0330

    added some indexes

--- cmd/migrate/migrations/init.sql
+++ cmd/migrate/migrations/init.sql
@@ -41,3 +41,17 @@ CREATE TABLE IF NOT EXISTS followers (
         Foreign Key (user_id) REFERENCES users (id) ON DELETE CASCADE,
         Foreign Key (follower_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE Extention IF NOT EXISTS pg_trgm;
+
+CREATE INDEX IF NOT EXISTS idx_comments_content ON comments USING gin (content gin_trgm_ops);
+
+CREATE INDEX IF NOT EXISTS idx_posts_title ON posts USING gin (title gin_trgm_ops);
+
+CREATE INDEX IF NOT EXISTS idx_posts_tags ON posts USING gin (tags);
+
+CREATE INDEX IF NOT EXISTS idx_users_username ON users (username);
+
+CREATE INDEX IF NOT EXISTS idx_posts_user_id ON posts (user_id);
+
+CREATE INDEX IF NOT EXISTS idx_comments_post_id ON comments (post_id);
